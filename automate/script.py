2a03c77d49f6fa89a53651c11fe6e196efd5fe76
Antonin <antonin.joulie@ynov.com>
2020-04-26 19:13:54 +0200

    Ajout timer

--- automate/script.py
+++ automate/script.py
@@ -4,79 +4,75 @@
 
 # //Python 3.7.x
 
+import threading
 import json
 from datetime import datetime
 import random
 
-today = datetime.now()
-date = today.strftime("%d-%m-%Y")
+def launch_automate():
+    threading.Timer(60.0, launch_automate).start()  # called every minute
+    print("Automate en cours de fonctionnement...")
 
-numero_unite = 1
+    today = datetime.now()
+    date = today.strftime("%d-%m-%Y")
 
-while numero_unite < 6:
+    numero_unite = 1
 
-    json_name = "json/paramunite_{}_{}.json".format(numero_unite, date)
+    while numero_unite < 6:
+
+        json_name = "json/paramunite_{}_{}.json".format(numero_unite, date)
+        print("Creation fichier paramunite_{}_{}.json".format(numero_unite, date))
+
+        numero_automate = 1
+
+        data = {}
+        data["unites"] = []
+        while numero_automate < 11:
+
+            type_automate = ['0X0000BA20', '0X0000BA2F']
+            temperature_cuve = round(random.uniform(2.5, 4), 1)
+            temperature_exterieur = round(random.uniform(8, 14), 1)
+            poids_lait_cuve = random.randint(3512, 4607)
+            poids_produit_fini = 1
+            pH = round(random.uniform(6.8, 7.2), 1)
+            K = random.randint(35, 47)
+            NaCl = round(random.uniform(1, 1.7), 1)
+            salmonelle = random.randint(17, 37)
+            Ecoli = random.randint(35, 49)
+            listeria = random.randint(28, 54)
+
+            data["unites"].append({
+                'name': "unite_{}".format(numero_unite),
+                'automates': [{
+                    'name': "automate_{}".format(numero_automate),
+                    'infos': [{
+                        'numero_unite': numero_unite,
+                        'numero_automate': numero_automate,
+                        'type_automate': random.choice(type_automate),
+                        'temperature_cuve': temperature_cuve,
+                        'temperature_exterieur': temperature_exterieur,
+                        'poids_lait_cuve': poids_lait_cuve,
+                        'poids_produit_fini': poids_produit_fini,
+                        'pH': pH,
+                        'K': K,
+                        'NaCl': NaCl,
+                        'salmonelle': salmonelle,
+                        'Ecoli': Ecoli,
+                        'listeria': listeria
+                    }]
+                }]
+            })
+            numero_automate += 1
+
+        numero_automate = 1
+
+        with open(json_name, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        numero_unite += 1
 
     numero_automate = 1
+    print('Data recuperee !')
 
-    data = {}
-    data["unites"] = []
-    while numero_automate < 11:
-        type_automate = ['0X0000BA20', '0X0000BA2F']
-        temperature_cuve = round(random.uniform(2.5, 4), 1)
-        temperature_exterieur = round(random.uniform(8, 14), 1)
-        poids_lait_cuve = random.randint(3512, 4607)
-        poids_produit_fini = 1
-        pH = round(random.uniform(6.8, 7.2), 1)
-        K = random.randint(35, 47)
-        NaCl = round(random.uniform(1, 1.7), 1)
-        salmonelle = random.randint(17, 37)
-        Ecoli = random.randint(35, 49)
-        listeria = random.randint(28, 54)
-        # data["unite_{}_automate_{}".format(numero_unite, numero_automate)] = []
-        # data["unite_{}_automate_{}".format(numero_unite,numero_automate)].append({
-        #     'numero_unite': numero_unite,
-        #     'numero_automate': numero_automate,
-        #     'type_automate': random.choice(type_automate),
-        #     'temperature_cuve': temperature_cuve,
-        #     'temperature_exterieur': temperature_exterieur,
-        #     'poids_lait_cuve': poids_lait_cuve,
-        #     'poids_produit_fini': poids_produit_fini,
-        #     'pH': pH,
-        #     'K': K,
-        #     'NaCl': NaCl,
-        #     'salmonelle': salmonelle,
-        #     'Ecoli': Ecoli,
-        #     'listeria': listeria
-        # })
-        data["unites"].append({
-            'name': "unite_{}".format(numero_unite),
-            'automates': [{
-                'name': "automate_{}".format(numero_automate),
-                'infos': [{
-                    'numero_unite': numero_unite,
-                    'numero_automate': numero_automate,
-                    'type_automate': random.choice(type_automate),
-                    'temperature_cuve': temperature_cuve,
-                    'temperature_exterieur': temperature_exterieur,
-                    'poids_lait_cuve': poids_lait_cuve,
-                    'poids_produit_fini': poids_produit_fini,
-                    'pH': pH,
-                    'K': K,
-                    'NaCl': NaCl,
-                    'salmonelle': salmonelle,
-                    'Ecoli': Ecoli,
-                    'listeria': listeria
-                }]
-            }]
-        })
-        numero_automate += 1
 
-    numero_automate= 1
-
-    with open(json_name, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
-
-    numero_unite += 1
-
-numero_automate = 1
+launch_automate()
