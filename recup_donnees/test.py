d738144096a0de794bc127ac5f92f092c6ece58b
Moi <valentin.dezat@gmail.com>
2020-04-26 21:50:42 +0200

    Ajout script test d'insertion dans la base

--- recup_donnees/test.py
+++ recup_donnees/test.py
@@ -1,14 +1,43 @@
-import pymysql
+import pymysql.cursors
+import random
 
-conn = pymysql.connect(host  ="localhost",
-                            user   ="root",
-                            port = 3308,
-                            password ='',
-                            db ='testdevops')
+# Connect to the database
+conn = pymysql.connect(host='localhost',
+                             user='root',
+                             port=3308,
+                             password='',
+                             db='testdevops',
+                             cursorclass=pymysql.cursors.DictCursor)
 
-myCursor = conn.cursor()
 
-myCursor.execute("INSERT INTO automate(num unit, num autom, mesure ph, mesure k+) VALUES(1,1,3.5,5);")
+try:
+    with conn.cursor() as cursor:
+        numero_unite = 1
+        numero_automate = 1
+        while numero_unite < 6:
+            while numero_automate < 11:
+                
+                temperature_cuve = round(random.uniform(2.5, 4), 1)
+                temperature_exterieur = round(random.uniform(8, 14), 1)
+                poids_lait_cuve = random.randint(3512, 4607)
+                poids_produit_fini = 1
+                pH = round(random.uniform(6.8, 7.2), 1)
+                K = random.randint(35, 47)
+                NaCl = round(random.uniform(1, 1.7), 1)
+                salmonelle = random.randint(17, 37)
+                Ecoli = random.randint(35, 49)
+                listeria = random.randint(28, 54)
+                # Create a new record
+                sql = "INSERT INTO `automate` (`numero_unite`, `numero_automate`, `type_automate`, `temperature_cuve`, `temperature_exterieur`, `poids_lait_cuve`, `poids_produit_fini`, `pH`, `K`, `NaCl`, `salmonelle`, `Ecoli`, `listeria`) VALUES (%s,%s,%s, %s,%s,	%s, %s,	%s,	%s, %s,	%s,	%s,	%s)"
+                cursor.execute(sql, (numero_unite,	numero_automate,"0X0000BA20", temperature_cuve, temperature_exterieur, poids_lait_cuve, poids_produit_fini, pH, K, NaCl, salmonelle, Ecoli, listeria))
+                numero_automate += 1
 
-conn.commit()
-conn.close()
+            numero_automate= 1
+            numero_unite += 1
+
+
+            conn.commit()
+
+
+finally:
+    conn.close()
