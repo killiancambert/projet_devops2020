c546d8349a69dd9dbc7ba3e5502e38fcd1dea634
killiancambert <killian.cambert@gmail.com>
2020-05-29 04:32:01 +0200

    commit before adding vuetify

---
+++ index/src/components/Chart.vue
@@ -0,0 +1,53 @@
+<script>
+import { Line, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  name: "Chart",
+  props: {
+    chartData: {
+      type: Object,
+      required: false
+    }
+  },
+  data() {
+    return {
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false
+              }
+            }
+          ]
+        },
+        legend: {
+          display: true,
+          position: "top",
+          labels: {
+            fontSize: 15
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        fill: false
+      }
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
---
+++ index/src/components/Details.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="selection">
+      <v-select v-model="defaultData" :options="options" @input="request()" :clearable="false"></v-select>
+    </div>
+    <line-chart :styles="chartStyle" :chart-data="datacollection" />
+    <div class="button">
+      <button>
+    </div>
+  </div>
+</template>
